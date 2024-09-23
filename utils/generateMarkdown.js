63ab42586e7b9e0fba58c2add45265e8ffd4da4b
vluu11 <luuvuong11@gmail.com>
2024-09-23 16:16:39 -0700

    added a markdown generator

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,6 +12,37 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+  let readMeContent =
+  
+`# ${data.title}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+## Credits 
+
+${data.credits}
+
+## License
+
+`;
+  return readMeContent;
 
 }
 
