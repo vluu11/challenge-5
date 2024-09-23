a588f8f84595d9079bf544c72f5fba76e6d3fdf0
vluu11 <luuvuong11@gmail.com>
2024-09-23 16:27:56 -0700

    added renderLicenseBadge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,18 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(!license){
+    return '';
+  }else{
+    return `![GitHub license](https://img.shields.io/badge/license-ISC-blue.svg)`
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
