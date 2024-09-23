d61c82a6ccc805df837dd19287ed97f68ee8d817
vluu11 <luuvuong11@gmail.com>
2024-09-23 16:11:25 -0700

    added questions array

---
+++ index.js
@@ -0,0 +1,67 @@
+// TODO: Include packages needed for this application
+import inquirer from "inquirer";
+import fs from "fs";
+import generateMarkdown from "./utils/generateMarkdown.js";
+
+// TODO: Create an array of questions for user input
+const questions = [
+{
+    type: 'input',
+    message: 'Enter project title',
+    name: 'title'
+},
+
+{
+    type: 'input',
+    message: 'Enter a description of your project.',
+    name: 'description'
+},
+
+{
+    type: 'input',
+    message: 'Add instructions for installation.',
+    name: 'installation'
+},
+
+{
+    type: 'input',
+    message: 'Add details on usage',
+    name: 'usage'
+},
+
+{
+    type: 'input',
+    message: 'Enter credits',
+    name: 'credits'
+},
+
+{
+    type: 'list',
+    message: 'Choose a license',
+    choices: ['Apache License 2.0', 'MIT License'],
+    name: 'license'
+},
+
+{
+    type: 'confirm',
+    name: 'continuePrompt',
+    message: 'Do you want to add another input?',
+    default: false,
+},
+
+]; 
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, response) {
+
+
+
+}
+
+// TODO: Create a function to initialize app
+function init() {
+   
+}
+
+// Function call to initialize app
+init();
