9899cf9bb08dc77f52cbe417ecd7bbb2c19579ea
vluu11 <luuvuong11@gmail.com>
2024-09-25 14:20:27 -0700

    filled out the readme

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -11,7 +11,7 @@ function renderLicenseBadge(license) {
       case 'Apache License 2.0':
         return `[License: Apache](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`
       
-      default:
+      default: 
     }
       
   }
@@ -45,8 +45,6 @@ function generateMarkdown(data) {
 
 `# ${data.title}
 
-${renderLicenseBadge(data.license)}
-
 ## Description
 
 ${data.description}
@@ -57,6 +55,9 @@ ${data.description}
 - [Usage](#usage)
 - [Credits](#credits)
 ${renderLicenseLink(data.license)}
+- [Features](#features)
+- [Contributions](#contribute)
+- [Contributions](#tests)
 
 ## Installation
 
@@ -71,53 +72,70 @@ ${data.usage}
 ${data.credits}
 
 ${renderLicenseSection(data.license)}
+${addLicenseText(data.license)}
 
-`;
+## Badges
 
-if(data.license === 'MIT License'){
-  const mitLicense =`Copyright (c) 2024
+${renderLicenseBadge(data.license)}
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+## Features
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+${data.features}
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-  `
-  readMeContent = `${readMeContent}${mitLicense}`
+## How to Contribute
 
-}else{
-  const apacheLicense = `Copyright 2024
+${data.contribute}
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+## Tests
 
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.`
-
-  readMeContent = `${readMeContent}${apacheLicense}`
-
-  }
+${data.tests}`;
 
 
   return readMeContent;
+  
+
+}
+
+function addLicenseText(data){let myLicense = null;
+
+  if(data.license === 'MIT License'){
+  return `Copyright (c) 2024
+  
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+  
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+  
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+    `
+  
+  }else{
+  return `Copyright 2024
+  
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+  
+      http://www.apache.org/licenses/LICENSE-2.0
+  
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.`
+  
+    }
 
 }
 
